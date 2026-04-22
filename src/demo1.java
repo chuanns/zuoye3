ebdd060a00ebe4be7ef247e0881f80e523e4b7ba
chuanns <zhangkaichuan1201@163.com>
2026-04-22 10:34:00 +0800

    second

--- src/demo1.java
+++ src/demo1.java
@@ -1,6 +1,6 @@
 public class demo1 {
     public static void main(String[] args) {
-        System.out.println("hahaha");
+        System.out.println("ccccc");
         System.out.println("llll");
     }
 }
